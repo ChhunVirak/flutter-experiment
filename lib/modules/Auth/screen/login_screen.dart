20988f0353793af85a96e4dec5ae73377e61ef25
Chhoeung Chhun Virak <chhunvirakchhoeung@gmail.com>
2024-03-26 09:48:34 +0700

    gen login screen

--- lib/modules/Auth/screen/login_screen.dart
+++ lib/modules/Auth/screen/login_screen.dart
@@ -7,8 +7,10 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text('login Screen'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('login Screen'),
+      ),
       body: Container(),
     );
   }
